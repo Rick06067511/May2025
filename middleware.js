36e8db56a03f76dca61475044a6b9ecc81d85a8e
Rick06067511 <gillett2@hotmail.com>
2025-03-14 11:09:23 +0000

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,15 +1,18 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(req) {
-    const password = 'mysecurepassword'; // Change this to your own password
+    const password = 'mysecurepassword'; // Change this
+    const encodedPassword = Buffer.from(password).toString('base64');
     const authHeader = req.headers.get('authorization');
 
-    if (!authHeader || authHeader !== `Basic ${btoa(password)}`) {
-        return new Response('Unauthorized', { status: 401, headers: {
-            'WWW-Authenticate': 'Basic realm="Secure Area"',
-        }});
+    if (!authHeader || authHeader !== `Basic ${encodedPassword}`) {
+        return new Response('Unauthorized', { 
+            status: 401, 
+            headers: { 'WWW-Authenticate': 'Basic realm="Secure Area"' }
+        });
     }
 
-    return NextResponse.next(); // Allow access if password is correct
+    return NextResponse.next();
 }
+
 
